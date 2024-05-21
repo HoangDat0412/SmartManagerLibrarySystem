caef891f3107d66fc1c5ebe0a9df0e26f7c32d5e
HoangDat0412 <hoang2811dat@gmail.com>
2024-05-21 19:34:27 +0700

    add manager user and manager book in admin page

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SmartManagerLibrarySystem.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,26 +12,196 @@ namespace SmartManagerLibrarySystem.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult ManageUsers()
+        public ActionResult ManageUsers(string searchString)
+        {
+            var users = from u in db.Users select u;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(s => s.UserName.Contains(searchString) || s.Email.Contains(searchString));
+            }
+
+            return View(users.ToList());
+        }
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            var user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("ManageUsers");
+        }
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,UserName")] ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingUser = db.Users.Find(user.Id);
+                existingUser.Email = user.Email;
+                existingUser.UserName = user.UserName;
+                db.Entry(existingUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ManageUsers");
+            }
+            return View(user);
+        }
+
+
+        public ActionResult ManagerBook(string searchString, string sortOrder)
+        {
+            ViewBag.NameSortParm = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.AuthorSortParm = sortOrder == "Author" ? "author_desc" : "Author";
+
+            var books = from b in db.Books select b;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(b => b.Name.Contains(searchString) || b.Author.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    books = books.OrderByDescending(b => b.Name);
+                    break;
+                case "Author":
+                    books = books.OrderBy(b => b.Author);
+                    break;
+                case "author_desc":
+                    books = books.OrderByDescending(b => b.Author);
+                    break;
+                default:
+                    books = books.OrderBy(b => b.Name);
+                    break;
+            }
+
+            return View(books.ToList());
+        }
+        // GET: Books/Details/5
+        public ActionResult BookDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        // GET: Books/Create
+        public ActionResult CreateBook()
         {
             return View();
         }
-        public ActionResult AddBook()
+
+        // POST: Books/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateBook([Bind(Include = "Id,Name,Author,Genre,Quantity,Description,Publisher,Image")] Books book)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("ManagerBook");
+            }
+
+            return View(book);
         }
-        public ActionResult EditBook()
+
+        // GET: Books/Edit/5
+        public ActionResult EditBook(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
         }
-        public ActionResult ManagerBook()
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook([Bind(Include = "BookId,Name,Author,Genre,Quantity,Description,Publisher,Image")] Books book)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ManagerBook");
+            }
+            return View(book);
         }
+
+        // GET: Books/Delete/5
+        public ActionResult DeleteBook(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("DeleteBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteBookConfirmed(int id)
+        {
+            var book = db.Books.Find(id);
+            db.Books.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("ManagerBook");
+        }
+
     }
 }
